b78dd2df12b22a6a9bed7969e693797ddec3e094
iam-max-thompson <max95thompson@gmail.com>
2024-05-05 17:56:30 -0400

    change max nodes

--- random_forest_run_1m.sh
+++ random_forest_run_1m.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=cpu
 #SBATCH --mem=20g
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=64
 #SBATCH --time 01:50:00
 #SBATCH -e ./utk_1m.e
 #SBATCH -o ./utk_1m.o
@@ -14,7 +14,7 @@ pwd
 module load r
 module list
 
-time Rscript random_forest_1m.R --args 32
+time Rscript random_forest_1m.R --args 64
 
 
 
