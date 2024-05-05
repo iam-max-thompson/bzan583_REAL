c310839d467c6ab175706e0b7c4d6cb7061a6be1
iam-max-thompson <max95thompson@gmail.com>
2024-05-05 15:06:48 -0400

    update for project

--- random_forest_parallel.R
+++ random_forest_parallel.R
@@ -2,12 +2,15 @@ library(parallel)                                       #<<
 library(randomForest)
 
 library(data.table)
-audio_features <- fread("/projects/bckj/Team2/data/Spotify/audio_features.csv")
+audio_features <- fread("/projects/bckj/Team2/data/Spotify/MAIN.csv")
 
+#audio_features <- fread("/Users/max/Downloads/spotify_working_data/MAIN.csv")
 set.seed(123)
 
 # sample 100,000 rows
-sub_audio_features <- audio_features[sample(nrow(audio_features), 100000, replace = FALSE), , drop = FALSE]
+sub_audio_features <- audio_features[sample(nrow(audio_features), 1000, replace = FALSE), , drop = FALSE]
+
+#sub_audio_features <- audio_features
 
 
 print("data loaded succesfully. correct wd")
@@ -27,7 +30,12 @@ test = sub_audio_features[i_test, ]
 nc = as.numeric(commandArgs(TRUE)[2])
 #nc <-  12
 ntree = lapply(splitIndices(200, nc), length)            #<<
-rf = function(x, train) randomForest(danceability ~ energy + loudness + speechiness + tempo + time_signature + valence, train, ntree=x, norm.votes = FALSE) #<<
+rf = function(x, train) randomForest(popularity ~ energy + loudness + 
+                                       speechiness + tempo + time_signature + 
+                                       valence + bpm + key + liveness + duration_ms +
+                                       release_year +  instrumentalness +
+                                       danceability + acousticness +
+                                       mode, train, ntree=x, norm.votes = FALSE) #<<
 rf.out = mclapply(ntree, rf, train = train, mc.cores = nc)      #<<
 rf.all = do.call(combine, rf.out)                        #<<
 
@@ -37,10 +45,25 @@ cpred = mclapply(crows, rfp, mc.cores = nc)              #<<
 pred = do.call(c, cpred)                                 #<<
 
 
-rmse <- sqrt(mean((test$danceability - pred)^2))
+rmse <- sqrt(mean((test$popularity - pred)^2))
 cat("RMSE:", rmse, "\n")
 
 end_time <- Sys.time()
 
+
+# grab ten most popular songs from release year 2023 no dplyr just base r
+test$pred <- round(pred, 1)
+
+# select only track_title, popularity, release_date and pred columns
+to_use <- test[, c("track_title", "popularity", "release_date", "pred")]
+
+top_ten <- to_use[order(to_use$popularity, decreasing = TRUE), ][1:20, ]
+
+# print top 
+print("top 20 songs:")
+top_ten
+
 # print time it took to run
 round(end_time - start_time, 2)
+
+
--- random_forest_run.sh
+++ random_forest_run.sh
@@ -4,8 +4,8 @@
 #SBATCH --partition=cpu
 #SBATCH --mem=20g
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=16
-#SBATCH --time 00:50:00
+#SBATCH --cpus-per-task=$(nproc)
+#SBATCH --time 04:50:00
 #SBATCH -e ./utk.e
 #SBATCH -o ./utk.o
 
@@ -14,9 +14,6 @@ pwd
 module load r
 module list
 
-time Rscript random_forest_serial.R
-time Rscript random_forest_parallel.R --args 1
 time Rscript random_forest_parallel.R --args 2
-time Rscript random_forest_parallel.R --args 4
-time Rscript random_forest_parallel.R --args 8
 time Rscript random_forest_parallel.R --args 16
+time Rscript random_forest_parallel.R --args $(nproc)
