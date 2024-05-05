fda652bcbe5a49db14b39123bc86b3e34beb127a
iam-max-thompson <max95thompson@gmail.com>
2024-05-05 16:46:45 -0400

    tweaking forest run 2 sh

--- random_forest_run_2.sh
+++ random_forest_run_2.sh
@@ -2,17 +2,17 @@
 #SBATCH --job-name utk
 #SBATCH --account=bckj-delta-cpu
 #SBATCH --partition=cpu
-#SBATCH --mem=20g
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=124
+#SBATCH --mem=40g
+#SBATCH --nodes=2
+#SBATCH --cpus-per-task=64
 #SBATCH --time 04:50:00
-#SBATCH -e ./utk_backup.e
-#SBATCH -o ./utk_backup.o
+#SBATCH -e ./utk_2.e
+#SBATCH -o ./utk_2.o
 
 pwd
 
 module load r
 module list
 
-time Rscript random_forest_parallel.R --args 124
+time Rscript random_forest_parallel.R --args 64
 time Rscript random_forest_parallel.R --args 16
