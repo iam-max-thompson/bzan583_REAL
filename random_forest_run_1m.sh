7d044e7789beba0e5d962e3f5bff40e0ee5a5623
iam-max-thompson <max95thompson@gmail.com>
2024-05-06 11:23:00 -0400

    testing 1000

--- random_forest_run_1m.sh
+++ random_forest_run_1m.sh
@@ -6,8 +6,8 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=120
 #SBATCH --time 18:50:00
-#SBATCH -e ./utk_overnight_2.e
-#SBATCH -o ./utk_overnight_2.o
+#SBATCH -e ./utk_final.e
+#SBATCH -o ./utk_final.o
 
 pwd
 
@@ -15,8 +15,7 @@ module load r
 module list
 
 time Rscript random_forest_1m.R --args 120
-time Rscript random_forest_1m.R --args 16
-time Rscript random_forest_1m.R --args 2
+
 
 
 
