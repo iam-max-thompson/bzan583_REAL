2cd635f5ac41bd3ba7a96e205515a15ec2854e41
iam-max-thompson <max95thompson@gmail.com>
2024-05-05 21:09:15 -0400

    utk_1m_2

--- random_forest_run_1m.sh
+++ random_forest_run_1m.sh
@@ -2,19 +2,19 @@
 #SBATCH --job-name utk
 #SBATCH --account=bckj-delta-cpu
 #SBATCH --partition=cpu
-#SBATCH --mem=64g
+#SBATCH --mem= 96g
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=120
 #SBATCH --time 01:50:00
-#SBATCH -e ./utk_1m.e
-#SBATCH -o ./utk_1m.o
+#SBATCH -e ./utk_1m_2.e
+#SBATCH -o ./utk_1m_2.o
 
 pwd
 
 module load r
 module list
 
-time Rscript random_forest_1m.R --args 32
+time Rscript random_forest_1m.R --args 64
 
 
 
